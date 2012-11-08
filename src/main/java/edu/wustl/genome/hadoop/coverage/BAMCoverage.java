293e414164df26201bc7109d78b3ec80a50b6348
Brian Derickson <bdericks@genome.wustl.edu>
2012-11-08 10:57:17 -0600

    updated to produce avro output

--- src/main/java/edu/wustl/genome/hadoop/coverage/BAMCoverage.java
+++ src/main/java/edu/wustl/genome/hadoop/coverage/BAMCoverage.java
@@ -1,6 +1,5 @@
 package edu.wustl.genome.hadoop.coverage;
 
-import fi.tkk.ics.hadoop.bam.BAMInputFormat;
 import fi.tkk.ics.hadoop.bam.SAMRecordWritable;
 import fi.tkk.ics.hadoop.bam.AnySAMInputFormat;
 
@@ -8,86 +7,129 @@ import net.sf.samtools.SAMRecord;
 import net.sf.samtools.AlignmentBlock;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.List;
 import java.util.Iterator;
 import java.lang.String;
+import java.io.File;
+import java.util.List;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.conf.Configured;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericData.Record;
+import org.apache.avro.mapred.AvroReducer;
+import org.apache.avro.mapred.AvroJob;
+import org.apache.avro.mapred.AvroKey;
+import org.apache.avro.mapred.AvroValue;
+import org.apache.avro.mapred.AvroCollector;
+import org.apache.avro.mapred.Pair;
+import org.apache.avro.util.Utf8;
 
 public class BAMCoverage {
 
-    public static class BAMCoverageMapper
-            extends Mapper<LongWritable, SAMRecordWritable, Text, IntWritable> {
+    private static final Schema coverageSchema = Schema.parse(
+        "{ \"namespace\": \"edu.wustl.genome.hadoop.coverage\", " +
+        "\"type\" : \"record\", \"name\" : \"PerPositionCoverage\", \"fields\" : [ " +
+        "{\"name\" : \"chromosome\", \"type\" : \"string\" }, {\"name\" : \"position\", " +
+        "\"type\" : \"int\" }, { \"name\" : \"depth\", \"type\" : \"int\" } ] }"
+    );
 
-        private final static IntWritable one = new IntWritable(1);
-        private Text chrom_pos = new Text();
+    public static class BamCoverageMapper extends Mapper<LongWritable, SAMRecordWritable, AvroKey<String>, AvroValue<Integer>> {
 
-        public void map(LongWritable key, SAMRecordWritable value, Context context) 
+        private static final Integer one = new Integer(1);
+        private static final AvroValue<Integer> avroValue = new AvroValue<Integer>(one);
+        private AvroKey<String> avroKey = new AvroKey<String>();
+
+        @Override
+        public void map(LongWritable key, SAMRecordWritable value, Context context)
                 throws IOException, InterruptedException {
             SAMRecord record = value.get();
             String referenceName = record.getReferenceName();
             List<AlignmentBlock> alignmentBlocks = record.getAlignmentBlocks();
-            AlignmentBlock alignment_block;
+            AlignmentBlock alignmentBlock;
             Iterator<AlignmentBlock> itr = alignmentBlocks.iterator();
-            int reference_start;
-            int block_length;
+            int referenceStart;
+            int blockLength;
             int position;
 
-            while ( itr.hasNext() ) {
-                alignment_block = itr.next();
-                reference_start = alignment_block.getReferenceStart();
-                block_length = alignment_block.getLength();
-                for (position = reference_start; position < reference_start + block_length; position++) {
-                    chrom_pos.set(referenceName + ":" + String.valueOf(position));
-                    context.write(chrom_pos, one);
+            while (itr.hasNext()) {
+                alignmentBlock = itr.next();
+                referenceStart = alignmentBlock.getReferenceStart();
+                blockLength = alignmentBlock.getLength();
+                for (position = referenceStart; position < referenceStart + blockLength; position++) {
+                    String chrPosition = new String(referenceName + ":" + String.valueOf(position));
+                    avroKey.datum(chrPosition);
+                    context.write(avroKey, avroValue);
                 }
             }
         }
     }
 
-    public static class IntSumReducer 
-            extends Reducer<Text,IntWritable,Text,IntWritable> {
+    public static class AvroBamCoverageReducer
+            extends AvroReducer<Utf8, Integer, GenericRecord> {
 
-        private IntWritable result = new IntWritable();
-
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) 
-                throws IOException, InterruptedException {
+        @Override
+        public void reduce(Utf8 key, Iterable<Integer> iterable,
+                AvroCollector<GenericRecord> collector, Reporter reporter)
+                throws IOException {
             int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
+            Iterator<Integer> iterator = iterable.iterator();
+            while(iterator.hasNext()) {
+                Integer value = iterator.next();
+                sum += value.intValue();
             }
-            result.set(sum);
-            context.write(key, result);
+
+            GenericRecord record = new GenericData.Record(coverageSchema);
+            String[] parts = key.toString().split(":");
+            record.put("chromosome", parts[0]);
+            record.put("position", new Integer(parts[1]));
+            record.put("depth", sum);
+            collector.collect(record);
         }
     }
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
+        JobConf conf = new JobConf();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         if (otherArgs.length != 2) {
             System.err.println("Usage: BAMCoverage <in> <out>");
             System.exit(2);
         }
+
         Job job = new Job(conf, "BAM Coverage");
         job.setJarByClass(BAMCoverage.class);
-        job.setMapperClass(BAMCoverageMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setInputFormatClass(AnySAMInputFormat.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        BAMInputFormat.addInputPath(job, new Path(otherArgs[0]));
+
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        Path outputPath = new Path(otherArgs[1]);
+        outputPath.getFileSystem(conf).delete(outputPath);
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+        job.setMapperClass(BamCoverageMapper.class);
+        job.setInputFormatClass(AnySAMInputFormat.class);
+        Schema stringSchema = Schema.create(Schema.Type.STRING);
+        Schema intSchema = Schema.create(Schema.Type.INT);
+        Schema mapPairSchema = Pair.getPairSchema(stringSchema, intSchema);
+        AvroJob.setMapOutputSchema(conf, mapPairSchema);
+
+        AvroJob.setReducerClass(conf, AvroBamCoverageReducer.class);
+        AvroJob.setOutputSchema(conf, coverageSchema);
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
